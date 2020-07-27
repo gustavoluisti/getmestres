76d907af7fc2023e601e8811cef3f7738bafca3d
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-07-27 20:26:35 -0300

    teste

--- getMestres.Api/src/config/config.ts
+++ getMestres.Api/src/config/config.ts
@@ -1,4 +1,5 @@
 export default {
   port: process.env.PORT || 3333,
   secretyKey: process.env.SECRETYKEY || "fb0caa0b7b0792d46ebfb2ed1844c7cd",
+  publicRoutes: process.env.PUBLICROUTES || ["users/create", "users/auth"],
 };
